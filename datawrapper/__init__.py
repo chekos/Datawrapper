439cf21fc01ee923fee6d2bdcc09eb501e84033c
chekos <chekos@tacosdedatos.com>
2019-12-11 15:37:26 -0800

    bump version

--- datawrapper/__init__.py
+++ datawrapper/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Sergio Sanchez"""
 __email__ = 'chekos@tacosdedatos.com'
-__version__ = '0.1.1'
+__version__ = '0.2.0'
 
 import datawrapper.datawrapper as dw
 from IPython.display import HTML, Image
