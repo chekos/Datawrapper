fbc78877ed8cb945db3728683fb0f61a2706d854
chekos <chekos@tacosdedatos.com>
2019-12-17 09:48:46 -0800

    update to v4

--- setup.py
+++ setup.py
@@ -47,6 +47,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/chekos/datawrapper',
-    version='0.3.0',
+    version='0.4.0',
     zip_safe=False,
 )
