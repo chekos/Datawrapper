0998172608846b16c2aec3634b17f157f278dbc3
Sergio Sanchez <ssanchez@alluma.org>
2022-02-19 12:11:47 -0800

    order of imports

--- docs/conf.py
+++ docs/conf.py
@@ -20,9 +20,10 @@
 import os
 import sys
 
+sys.path.insert(0, os.path.abspath(".."))
+
 import datawrapper
 
-sys.path.insert(0, os.path.abspath(".."))
 
 
 # -- General configuration ---------------------------------------------
